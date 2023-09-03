9ebe120e0932b18fa2851ffdcf39ad78ce38b994
hyeonjaez <jhjh0022@naver.com>
2023-09-04 01:12:12 +0900

    Refactor: logger 추가 및 printMsg() 추가

--- src/main/java/com/nhnacademy/mart/Counter.java
+++ src/main/java/com/nhnacademy/mart/Counter.java
@@ -1,6 +1,11 @@
 package com.nhnacademy.mart;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Counter {
+
+    private final static Logger logger = LoggerFactory.getLogger(Counter.class);
 
     public int pay(Basket basket) {
         int totalPrice = 0;
@@ -8,7 +13,11 @@ public class Counter {
         for (Food food : basket.getFoods()) {
             totalPrice += food.getPrice();
         }
-        System.out.println("총 가격은 " + totalPrice + "원 입니다.");
+        printMsg(totalPrice);
         return totalPrice;
+    }
+
+    private void printMsg(int totalPrice) {
+        logger.info("총 가격은 " + totalPrice + "원 입니다.");
     }
 }
