2fafe0fccc9c79846526129bb6169d83a32c8f16
hyeonjaez <jhjh0022@naver.com>
2023-09-04 00:25:30 +0900

    FEAT: pay() 구현

--- src/main/java/com/nhnacademy/mart/Counter.java
+++ src/main/java/com/nhnacademy/mart/Counter.java
@@ -2,6 +2,13 @@ package com.nhnacademy.mart;
 
 public class Counter {
 
-    // TODO pay 메서드 구현 (카운터에서 계산 처리 메서드)
+    public int pay(Basket basket) {
+        int totalPrice = 0;
 
+        for (Food food : basket.getFoods()) {
+            totalPrice += food.getPrice();
+        }
+        System.out.println("총 가격은 " + totalPrice + "원 입니다.");
+        return totalPrice;
+    }
 }
