373bbaea12f4467bdc631feb559a91cf4e79ec75
hyeonjaez <jhjh0022@naver.com>
2023-09-04 01:52:15 +0900

    Test : basketTest2() 테스트 코드 작성

--- src/test/java/com/nhnacademy/mart/BasketTest.java
+++ src/test/java/com/nhnacademy/mart/BasketTest.java
@@ -1,6 +1,7 @@
 package com.nhnacademy.mart;
 
 import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -20,4 +21,28 @@ class BasketTest {
         Assertions.assertEquals(basket.getFoods().get(0).getName(), foods.get(0).getName());
     }
 
+    @Test
+    @DisplayName("add(), getFoods() 테스트 2")
+    void basketTest2() {
+        List<Food> foodsTest = List.of(
+                new Food("양배추", 10000),
+                new Food("닭가슴살", 2500),
+                new Food("프로틴 우유", 2300));
+
+        Basket basket = new Basket();
+        basket.add(new Food("양배추", 10000));
+        basket.add(new Food("닭가슴살", 2500));
+        basket.add(new Food("프로틴 우유", 2300));
+
+        for (int i = 0; i < basket.getFoods().size(); i++) {
+
+            Assertions.assertEquals(basket.getFoods().get(i).getName(), foodsTest.get(i).getName());
+            Assertions.assertEquals(basket.getFoods().get(i).getPrice(), foodsTest.get(i).getPrice());
+        }
+    }
+
 }
+
+
+
+
