a94067a7a8be9f5b81328f6048432b1ec04da6b5
hyeonjaez <jhjh0022@naver.com>
2023-09-04 02:13:58 +0900

    TEST: Customer 테스트 코드 작성

--- src/test/java/com/nhnacademy/mart/CustomerTest.java
+++ src/test/java/com/nhnacademy/mart/CustomerTest.java
@@ -1,4 +1,22 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.nhnacademy.mart;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class CustomerTest {
-  
+
+    @Test
+    @DisplayName("Customer 생성자 테스트 입니다")
+    void customerConstructorTest() {
+        BuyList buyList = new BuyList();
+        buyList.getItems().add(new BuyList.Item("양파", 2));
+        int money = 1000;
+
+        Customer customer = new Customer(buyList, money);
+
+        Assertions.assertEquals(customer.getMoney(), money);
+        Assertions.assertEquals(customer.getBuyList(), buyList);
+    }
+
 }
